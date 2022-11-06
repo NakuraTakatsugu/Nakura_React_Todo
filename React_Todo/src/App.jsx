5f7df80a15bdae1642f03955561500a79e336e19
NakuraTakatsugu <arukan.b.b@gmail.com>
2022-11-06 23:43:57 +0900

    [add]タスク追加、編集機能を追加

--- React_Todo/src/App.jsx
+++ React_Todo/src/App.jsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleOnChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleOnSubmit = () => {
+    if (!text) return;
+
+    const newTodo = {
+      value: text,
+      id: new Date().getTime(),
+    };
+
+    setTodos([newTodo, ...todos]);
+    setText('');
+  };
+
+  const handleOnEdit = (id = 0,value = '') => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.value = value;
+      }
+      return todo;
+    });
+
+    setTodos(newTodos);
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Todo App</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleOnSubmit();
+        }}
+      >
+        <input type="text" value={text} onChange={(e) => handleOnChange(e)} />
+        <input type="submit" value="追加" onSubmit={handleOnSubmit} />
+      </form>
+      <ul>
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <input
+                type="text"
+                value={todo.value}
+                onChange={(e) => handleOnEdit(todo.id,e.target.value)}
+              />
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  )
-}
-
-export default App
+  );
+};
