ebfc0d0679c35801595edca0d1946cf00567449e
NakuraTakatsugu <arukan.b.b@gmail.com>
2022-11-30 00:49:57 +0900

    add fetch method

--- React_Todo/src/utils/api.js
+++ React_Todo/src/utils/api.js
@@ -3,3 +3,21 @@ import { API_BASE_URL } from "../config";
 
 export const fetcher = (URL) =>
   axios.get(API_BASE_URL + URL).then((res) => res.data);
+
+export const creteTodo = (todo) => {
+  axios.get(`${API_BASE_URL}/todos`, {
+    title: todo.title,
+    isCompleted: todo.isCompleted,
+    isDiscarded: todo.isDiscarded,
+    isChecked:todo.isChecked,
+  })
+}
+
+export const updateTodo = (todo) => {
+  axios.put(`${API_BASE_URL}/todos/${id}`, {
+    title: todo.title,
+    isCompleted: todo.isCompleted,
+    isDiscarded: todo.isDiscarded,
+    isChecked:todo.isChecked,
+  })
+}
