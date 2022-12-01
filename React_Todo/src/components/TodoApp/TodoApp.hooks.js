1c5ee435caabf2e13faf44add134cd715117d43d
NakuraTakatsugu <arukan.b.b@gmail.com>
2022-12-01 20:55:50 +0900

    [fix]hooksの構造を修正

--- React_Todo/src/components/TodoApp/TodoApp.hooks.js
+++ React_Todo/src/components/TodoApp/TodoApp.hooks.js
@@ -1,76 +1,76 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
-export const useTodos = () => {
-  const [todos, setTodos] = useState(Array);
+// export const useTodos = () => {
+//   const [todos, setTodos] = useState(Array);
 
-  const handleOnSubmit = (e, text) => {
-    e.preventDefault();
+  // const handleOnSubmit = (e, text) => {
+  //   e.preventDefault();
 
-    if (!text) return;
+  //   if (!text) return;
 
-    const newTodo = {
-      value: text,
-      id: new Date().getTime(),
-      checked: false,
-      removed: false,
-    };
+  //   const newTodo = {
+  //     value: text,
+  //     id: new Date().getTime(),
+  //     checked: false,
+  //     removed: false,
+  //   };
 
-    setTodos([newTodo, ...todos]);
-  };
+  //   setTodos([newTodo, ...todos]);
+  // };
 
-  const handleOnEdit = (id = Number, value = String) => {
-    const deepCopy = todos.map((todo) => ({ ...todo }));
+  // const handleOnEdit = (id = Number, value = String) => {
+  //   const deepCopy = todos.map((todo) => ({ ...todo }));
 
-    const newTodos = deepCopy.map((todo) => {
-      if (todo.id === id) {
-        todo.value = value;
-      }
-      return todo;
-    });
+  //   const newTodos = deepCopy.map((todo) => {
+  //     if (todo.id === id) {
+  //       todo.value = value;
+  //     }
+  //     return todo;
+  //   });
 
-    console.log('=== Original todos ===');
-    todos.map((todo) => console.log(`id: ${todo.id}, value: ${todo.value}`));
+  //   console.log('=== Original todos ===');
+  //   todos.map((todo) => console.log(`id: ${todo.id}, value: ${todo.value}`));
 
-    setTodos(newTodos);
-  };
+  //   setTodos(newTodos);
+  // };
 
-  const handleOnCheck = (id = Number, checked = Boolean) => {
-    const deepCopy = todos.map((todo) => ({ ...todo }));
+  // const handleOnCheck = (id = Number, checked = Boolean) => {
+  //   const deepCopy = todos.map((todo) => ({ ...todo }));
 
-    const newTodos = deepCopy.map((todo) => {
-      if (todo.id === id) {
-        todo.checked = !checked;
-      }
-      return todo;
-    });
+  //   const newTodos = deepCopy.map((todo) => {
+  //     if (todo.id === id) {
+  //       todo.checked = !checked;
+  //     }
+  //     return todo;
+  //   });
 
-    setTodos(newTodos);
-  };
+  //   setTodos(newTodos);
+  // };
 
-  const handleOnRemove = (id = Number, removed = Boolean) => {
-    const deepCopy = todos.map((todo) => ({ ...todo }));
+  // const handleOnRemove = (id = Number, removed = Boolean) => {
+  //   const deepCopy = todos.map((todo) => ({ ...todo }));
 
-    const newTodos = deepCopy.map((todo) => {
-      if (todo.id === id) {
-        todo.removed = !removed;
-      }
-      return todo;
-    });
+  //   const newTodos = deepCopy.map((todo) => {
+  //     if (todo.id === id) {
+  //       todo.removed = !removed;
+  //     }
+  //     return todo;
+  //   });
 
-    setTodos(newTodos);
-  };
+  //   setTodos(newTodos);
+  // };
 
-  const handleOnEmpty = () => {
-    const newTodos = todos.filter((todo) => !todo.removed);
-    setTodos(newTodos);
-  };
+  // const handleOnEmpty = () => {
+  //   const newTodos = todos.filter((todo) => !todo.removed);
+  //   setTodos(newTodos);
+  // };
 
-  return {
-    todos,
-    handleOnSubmit,
-    handleOnEdit,
-    handleOnCheck,
-    handleOnRemove,
-    handleOnEmpty,
-  };
-};
+//   return {
+//     todos,
+//     handleOnSubmit,
+//     handleOnEdit,
+//     handleOnCheck,
+//     handleOnRemove,
+//     handleOnEmpty,
+//   };
+// };
