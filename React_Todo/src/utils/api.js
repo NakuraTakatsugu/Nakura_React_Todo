1233f9b69aaf7f59ab78b5d176bfe9ed59632017
NakuraTakatsugu <arukan.b.b@gmail.com>
2022-12-07 01:20:08 +0900

    [update]APIの追加(更新、完了、削除、復帰、完全削除)

--- React_Todo/src/utils/api.js
+++ React_Todo/src/utils/api.js
@@ -5,19 +5,40 @@ export const fetcher = (URL) =>
   axios.get(API_BASE_URL + URL).then((res) => res.data);
 
 export const createTodo = (todo) => {
-  axios.get(`${API_BASE_URL}/todos`, {
+  axios.post(`${API_BASE_URL}/todos`, {
     title: todo.title,
     isCompleted: todo.isCompleted,
     isDiscarded: todo.isDiscarded,
-    isChecked:todo.isChecked,
-  })
-}
+  });
+};
 
-export const updateTodo = (todo) => {
-  axios.put(`${API_BASE_URL}/todos/${id}`, {
-    title: todo.title,
-    isCompleted: todo.isCompleted,
-    isDiscarded: todo.isDiscarded,
-    isChecked:todo.isChecked,
-  })
-}
+export const updateTodo = (todo, value) => {
+  const updateTodo = { ...todo, title: value };
+
+  axios.put(`${API_BASE_URL}/todos/${todo.id}`, updateTodo);
+};
+
+export const completeTodo = (todo) => {
+  axios.put(`${API_BASE_URL}/todos/${todo.id}/complete`, {
+    ...todo,
+    isCompleted: true,
+  });
+};
+
+export const discardTodo = (todo) => {
+  axios.put(`${API_BASE_URL}/todos/${todo.id}/discard`, {
+    ...todo,
+    isDiscarded: true,
+  });
+};
+
+export const restoreTodo = (todo) => {
+  axios.put(`${API_BASE_URL}/todos/${todo.id}/restore`, {
+    ...todo,
+    isRestored:true,
+  });
+};
+
+export const deleteTodo = (todo) => {
+  axios.delete(`${API_BASE_URL}/todos/${todo.id}`);
+};
